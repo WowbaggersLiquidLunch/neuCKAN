833e9742b66ec10f0a1e1811ed05eff7976964da
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2019-10-31 18:53:11 -0400

    Initial Commit

---
+++ neuCKAN/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  neuCKAN
+//
+//  Created by 冀卓疌 on 19-10-31.
+//  Copyright © 2019 Wowbagger & His Liquid Lunch. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
