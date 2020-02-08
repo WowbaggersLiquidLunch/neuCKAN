cce72901114e068528f3a2dd0c34c6c9f557b295
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-02-07 19:31:31 -0500

    conform to Collection
    fix access levels
    
    fully fledge the struct
    
    clean up documentation

--- neuCKAN/Models/Mod.swift
+++ neuCKAN/Models/Mod.swift
@@ -9,72 +9,47 @@
 import Foundation
 
 /**
-A _mod_.
+A collection of a _mod_'s releases arranged in reverse chronological order.
 
-A mod is defined by its ["identifier" attribute][identifier] in a .ckan file. The attribute is equivalent to the `id` field in a `Mod` struct.
-
-A `Mod` struct aggregates all _releases_ of the same mod under/with the same `id`.
+A mod is defined by its ["identifier" attribute][identifier] in a .ckan file. The attribute is equivalent to the `id` field in a `Mod` struct. A `Mod` instance aggregates all _releases_ of the same mod under/with the same `id`.
 
 A mod release is a version or distribution of a mod released to public on [CKAN][0]. Each mod release has a version number unique from its sibling releases of the same mod. For example, the following are 3 releases of the same mod "Farram Aerospace Reseach":
 - [Ferram Aerospace Research v0.15.9.1	"Liepmann"][1]
 - [Ferram Aerospace Research v0.15.9	"Liebe"][2]
 - [Ferram Aerospace Research v0.15.8.1	"Lewis"][3]
 
-A `Release` instance contains all metadata of a mod release, as made available on [the CKAN metadata repository][4]. In a way of speech, the structure of a `Release` instance conforms to [the CKAN metadate specification][5] \(currently v1.26), which is put concisely in [the specification's json schema][6].
+The read-only `releases` instance property is an array of all the releases. The releases are sorted to reverse chronological order by comparison of their versions whenever `releases` changes.
 
-A `Mod` instance contains all releases of the same mod, as identified by the same `id`.
+- See Also: `Release`.
 
 [identifier]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md#identifier
 [0]: https://github.com/KSP-CKAN/CKAN
 [1]: https://github.com/ferram4/Ferram-Aerospace-Research/releases/tag/v0.15.9.1_Liepmann
 [2]: https://github.com/ferram4/Ferram-Aerospace-Research/releases/tag/v0.15.9_Liebe
 [3]: https://github.com/ferram4/Ferram-Aerospace-Research/releases/tag/v0.15.8.1_Lewis
-[4]: https://github.com/KSP-CKAN/CKAN-meta
-[5]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md
-[6]: https://github.com/KSP-CKAN/CKAN/blob/master/CKAN.schema
 */
 struct Mod: Hashable, Codable, Identifiable {
 	
 	/**
-	Instantiates `Mod` from the given `Release` instance.
+	Instantiates a mod release collection from the given release.
 	
 	- Parameter release: The `Release` instance to from which to instantiates `Mod`.
 	*/
-	init(_ release: Release) {
+	init(with release: Release) {
 		self.id = release.id
 		self.releases = [release]
 	}
 	
 	/**
-	Mod name.
-	
-	This is the human readable name of the mod, and may contain any printable characters.
-	
-	This is equivalent to the ["name" attribute][0] in a .ckan file.
-	
-	For example:
-	- "Ferram Aerospace Research (FAR)"
-	- "Real Solar System".
-	
-	- Note: This is a computed instance property from a stored `name` property in the corresponding instance of `Release` structure.
-	
-	[0]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md#name
+	Instantiates a mod release collection from inserting the given release into the given collection.
 	*/
-	var name: String { releases.max(by: { $0.version > $1.version })?.name ?? "mod name does not exist" }
+	init(updatingFrom mod: Mod, with release: Release) {
+		var modCopy = mod
+		self = modCopy.inserted(release)
+	}
 	
 	/**
-	A short, one line description of the mod and what it does.
-	
-	This is equivalent to the ["abstract" attribute][0] in a .ckan file.
-	
-	- Note: This is a computed instance property from a stored `abstruct` property in the corresponding instance of `Release` structure.
-	
-	[0]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md#abstract
-	*/
-	var abstract: String { releases.max(by: { $0.version > $1.version })?.abstract ?? "mod abstract dores not exist" }
-	
-	/**
-	The globally unique identifier for the mod.
+	The mod's read-only globally unique identifier.
 	
 	This is equivalent to the ["identifier" attribute][0] in a .ckan file.
 	
@@ -88,30 +63,48 @@ struct Mod: Hashable, Codable, Identifiable {
 	
 	The identifier is both case sensitive for machines, and unique regardless of capitalization for human consumption and case-ignorant systems. For example: the hypothetical identifier `"MyMod"` must always be expressed as `"MyMod"` everywhere, but another module cannot assume the `"mymod"` identifier.
 	
-	- Attention: If the mod would generate a `FOR` pass in ModuleManager, then the identifier should be same as the `"ModuleManager"` name. For most mods, this means the identifier should be the name of the directory in `GameData/` in which the mod would be installed, or the name of the `.dll` with any version and the ".dll" suffix removed.
-	
 	[0]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md#identifier
 	*/
 	let id: String
 	
 	/**
-	A collection of _mod releases_ of the same mod, with their associated _version numbers_.
+	The mod's name.
+	
+	This is the human readable name of the mod, and may contain any printable characters.
+	
+	This is equivalent to the ["name" attribute][0] in a .ckan file.
+	
+	For example:
+	- "Ferram Aerospace Research (FAR)"
+	- "Real Solar System".
+	
+	[0]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md#name
+	*/
+	var name: String { releases.max(by: { $0.version > $1.version })?.name ?? "mod name does not exist" }
+	
+	/**
+	A short, one line description of the mod and what it does.
+	
+	This is equivalent to the ["abstract" attribute][0] in a .ckan file.
+	
+	[0]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md#abstract
+	*/
+	var abstract: String { releases.max(by: { $0.version > $1.version })?.abstract ?? "mod abstract dores not exist" }
+	
+	/**
+	A collection of _mod releases_ of the same mod.
 	
 	A mod release is a version or distribution of a mod released to public on [CKAN][0]. Each mod release has a version number unique from its sibling releases of the same mod. For example, the following are 3 releases of the same mod "Farram Aerospace Reseach":
 	- [Ferram Aerospace Research v0.15.9.1	"Liepmann"][1]
 	- [Ferram Aerospace Research v0.15.9	"Liebe"][2]
 	- [Ferram Aerospace Research v0.15.8.1	"Lewis"][3]
-	
-	A `Release` instance contains all metadata of a mod release, as made available on [the CKAN metadata repository][4]. In a way of speech, the structure of a `Release` instance conforms to [the CKAN metadate specification][5] \(currently v1.26), which is put concisely in [the specification's json schema][6].
+		
+	- See Also: `Release`.
 	
 	[0]: https://github.com/KSP-CKAN/CKAN
 	[1]: https://github.com/ferram4/Ferram-Aerospace-Research/releases/tag/v0.15.9.1_Liepmann
 	[2]: https://github.com/ferram4/Ferram-Aerospace-Research/releases/tag/v0.15.9_Liebe
 	[3]: https://github.com/ferram4/Ferram-Aerospace-Research/releases/tag/v0.15.8.1_Lewis
-	[4]: https://github.com/KSP-CKAN/CKAN-meta
-	[5]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md
-	[6]: https://github.com/KSP-CKAN/CKAN/blob/master/CKAN.schema
-	[7]: https://github.com/KSP-CKAN/CKAN/blob/master/Spec.md#version
 	*/
 	private(set) var releases: [Release] {
 		didSet {
@@ -120,9 +113,11 @@ struct Mod: Hashable, Codable, Identifiable {
 	}
 	
 	/**
-	Insert a mod release into a mods array.
+	Add a new release into the mod.
 	
-	The `Release` instance is only inserted if it doesn't already exists.
+	The release is only inserted if it doesn't already exists.
+	
+	- Parameter release: The release instance to be added in this collection.
 	
 	- Complexity: O(1) on average, over many calls to insert(_:) on the same mod.
 	*/
@@ -131,5 +126,88 @@ struct Mod: Hashable, Codable, Identifiable {
 			releases.append(release)
 		}
 	}
+	
+	/**
+	Add a new release into the mod, then returns itself.
+	
+	The release is only inserted if it doesn't already exists.
+	
+	- Parameter release: The release instance to be added in this collection.
+	
+	- Returns: The mod release collection itself.
+	
+	- Complexity: O(1) on average, over many calls to insert(_:) on the same mod.
+	*/
+	mutating func inserted(_ release: Release) -> Mod {
+		insert(release)
+		return self
+	}
+	
+	/**
+	Accesses the mod release of the specified version.
+	
+	An existing release can be read from, and a new release writen to the mod release collection through this subscript.
+	
+	- Parameter version: The version of the mod release.
+	
+	- Returns: The mod release of the specified version.
+	
+	- Complexity: O(1)
+	*/
+	subscript(version: Version) -> Release? {
+		get {
+			releases.first(where: { $0.version == version })
+		}
+		set(newRelease) {
+			releases.removeAll(where: { $0.version == version })
+			if let newRelease = newRelease {
+				self.insert(newRelease)
+			}
+		}
+	}
 }
 
+//	MARK: - Collection Conformance
+
+extension Mod: Collection {	
+	
+	typealias Index = Array<Any>.Index
+	
+	/**
+	The position of the latest release in a nonempty mod rrelease collection.
+	
+	If the mod has no releases, `startIndex` is equal to `endIndex`.
+	*/
+	var startIndex: Index { releases.startIndex }
+	
+	/**
+	The mod’s “past the end” position—i.e. the position one greater than the last valid subscript argument.
+	
+	When you need a range that includes the earliest release of a mod, use the half-open range operator (`..<`) with `endIndex`. The `..<` operator creates a range that doesn’t include the upper bound, so it’s always safe to use with `endIndex`.
+	
+	If the mod has no releases, `endIndex` is equal to `startIndex`.
+	*/
+	var endIndex: Index { releases.endIndex }
+	
+	/**
+	Accesses the mod release at the specified reverse-chronological position.
+	
+	This subscript provides read-only access. For write access, use `subscript(version:)`.
+	
+	- Parameter position: The position of the mod release to access. `position` must be a valid index of the mod release collection that is not equal to the `endIndex` property.
+	
+	- Returns: The mod release at the specified reverse-chronological position.
+	
+	- Complexity: O(1)
+	*/
+	subscript(position: Index) -> Release? { releases[position] }
+	
+	/**
+	Returns the position immediately after the given position.
+	
+	- Parameter position: A valid polition of the mod release collection. `position` must be less than `endIndex`.
+	
+	- Returns: The position value immediately after `position.`
+	*/
+	func index(after position: Index) -> Index { releases.index(after: position) }
+}
