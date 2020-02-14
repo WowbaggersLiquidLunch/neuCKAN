40fe5533641e5eea671bb9ed38a92abcc9dfb41b
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-02-14 17:28:25 -0500

    add Target struct

---
+++ neuCKAN/Models/Target.swift
@@ -0,0 +1,55 @@
+//
+//  Target.swift
+//  neuCKAN
+//
+//  Created by you on 20-01-13.
+//  Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
+//
+
+import Foundation
+import Cocoa
+
+struct Target: Hashable {
+	
+	init(version: Version, path: URL) {
+		self.version = version
+		self.path = path
+		self.badge = NSImage(named: "KSP \(version[..<2]) Patch") ?? NSImage(named: "KSP Logo Red")!
+	}
+	
+	var filterDidChange: Bool = false
+	//	TODO: Add stored array of mods.
+	//	Stored because of faster runtime.
+	//	TODO: Add stored filter with didset observer.
+	//	The mods array changes when filter changes.
+	var mods: [Mod] { Array(Synecdoche.mods) }
+	let version: Version
+	let badge: NSImage
+	let path: URL
+	var gameDataPath: URL {
+		path.appendingPathComponent("GameData")
+	}
+}
+
+extension Target {
+	
+	init(version: String, path: URL) {
+		self.init(version: Version(version), path: path)
+	}
+	
+	init(version: Double, path: URL) {
+		self.init(version: Version(version), path: path)
+	}
+	
+	init(version: String, path: String) {
+		self.init(version: Version(version), path: URL(fileURLWithPath: path))
+	}
+	
+	init(version: Double, path: String) {
+		self.init(version: Version(version), path: URL(fileURLWithPath: path))
+	}
+	
+	init(version: Version, path: String) {
+		self.init(version: version, path: URL(fileURLWithPath: path))
+	}
+}
