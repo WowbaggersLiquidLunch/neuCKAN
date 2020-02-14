ddb6d97d3dce938438b2891ad6a623b906cf0ee3
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-02-14 17:27:40 -0500

    add empty Filter struct

---
+++ neuCKAN/Models/Filter.swift
@@ -0,0 +1,13 @@
+//
+//  Filter.swift
+//  neuCKAN
+//
+//  Created by you on 20-01-20.
+//  Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
+//
+
+import Foundation
+
+struct Filter {
+	
+}
