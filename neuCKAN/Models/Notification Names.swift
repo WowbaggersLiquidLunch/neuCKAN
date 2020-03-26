60281b7cb034dc579b573a6ed0db9b3d0311ccf5
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-03-26 01:02:08 -0400

    extend Notification.Name

---
+++ neuCKAN/Models/Notification Names.swift
@@ -0,0 +1,16 @@
+//
+//  Notification Names.swift
+//  neuCKAN
+//
+//  Created by 冀卓疌 on 20-02-23.
+//  Copyright © 2020 Wowbagger & His Liquid Lunch. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+	static let targetsDidUpdate = Notification.Name("targetsDidUpdate")
+	static let modsDidUpdate = Notification.Name("modsDidUpdate")
+	static let targetsSelectionDidChange = Notification.Name("targetsSelectionDidChange")
+	
+}
