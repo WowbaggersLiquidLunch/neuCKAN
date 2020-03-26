47fd969d2360e1dea5a74b88ca7cce5f4adad475
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-03-26 00:53:09 -0400

    add preferences

---
+++ neuCKAN/Models/Preferences.swift
@@ -0,0 +1,22 @@
+//
+//  Preferences.swift
+//  neuCKAN
+//
+//  Created by you on 20-01-18.
+//  Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
+//
+
+import Foundation
+
+/**
+Temporary location for storing some preferences.
+*/
+struct Preferences {
+	static var databaseShouldReloadOnAppLaunching: Bool = true
+	static var updateMetadataOnLaunch: Bool = true
+	static var metadataParsingScheme: MetadataParsingScheme = .sequential
+	enum MetadataParsingScheme {
+		case sequential
+		case concurrent
+	}
+}
