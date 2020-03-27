3dcc859794de1af1c607abec43681215111a5e62
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-03-27 13:57:30 -0400

    add StatsViewController

---
+++ neuCKAN/Controllers/StatsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  StatsViewController.swift
+//  neuCKAN
+//
+//  Created by you on 20-01-11.
+//  Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
+//
+
+import Cocoa
+
+class StatsViewController: NSViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override var representedObject: Any? {
+		didSet {
+			// Update the view, if already loaded.
+		}
+	}
+	
+}
