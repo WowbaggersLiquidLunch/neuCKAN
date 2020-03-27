6c622f82576e8ccdc5c2399639d89c75055271c0
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-03-27 12:34:24 -0400

    add MainHorizontalSplitViewController

---
+++ neuCKAN/Controllers/MainHorizontalSplitViewController.swift
@@ -0,0 +1,24 @@
+//
+//  MainHorizontalSplitViewController.swift
+//  neuCKAN
+//
+//  Created by you on 20-02-14.
+//  Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
+//
+
+import Cocoa
+
+class MainHorizontalSplitViewController: NSSplitViewController {
+	
+	///	The horizontal split view containing mods view and stats view in neuCKAN's main window.
+	@IBOutlet weak var mainHorizontalSplitView: NSSplitView!
+	///	The split view item for stats view.
+	@IBOutlet weak var statsSplitViewItem: NSSplitViewItem!
+	
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
