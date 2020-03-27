83ee920f686418d18bdbdf3a79c7c04cd6601a8a
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-03-27 02:23:25 -0400

    add DetailsViewController

---
+++ neuCKAN/Controllers/DetailsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsViewController.swift
+//  neuCKAN
+//
+//  Created by you on 20-01-11.
+//  Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
+//
+
+import Cocoa
+
+class DetailsViewController: NSViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override var representedObject: Any? {
+		didSet {
+			// Update the view, if already loaded.
+		}
+	}
+	
+}
