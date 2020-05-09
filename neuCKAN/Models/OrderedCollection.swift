16a8141c72504817c0cc64c0d8aaa567c517215d
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-05-09 18:10:28 -0400

    overload Sequence's sorted() and sorted(by:)

--- neuCKAN/Models/OrderedCollection.swift
+++ neuCKAN/Models/OrderedCollection.swift
@@ -9,9 +9,25 @@
 import Foundation
 
 ///	An ordered collection.
-protocol OrderedCollection: Hashable, RandomAccessCollection, MutableCollection where Element: Comparable {
+protocol OrderedCollection: Hashable, RandomAccessCollection, MutableCollection {
+	///	Returns the elements of the ordered collection, sorted using the given predicate as the comparison between elements.
+	///	- Parameter areInIncreasingOrder: A predicate that returns `true` if its first argument should be ordered before its second argument; otherwise, `false`.
+	///	- Returns: A sorted ordered collection of the ordered collection’s elements.
+	@inlinable func sorted(by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows -> Self
 }
 
 extension OrderedCollection {
+	@inlinable func sorted(by areInIncreasingOrder: (Element, Element) throws -> Bool) rethrows -> Self {
+		var newOrdredCollection = self
+		try newOrdredCollection.sort(by: areInIncreasingOrder)
+		return newOrdredCollection
+	}
+}
 
+extension OrderedCollection where Element: Comparable {
+	/// Returns the elements of the ordered collection, sorted.
+	/// - Returns: A sorted ordered collection of the ordered collection’s elements.
+	@inlinable func sorted() -> Self {
+		return sorted(by: <)
+	}
 }
