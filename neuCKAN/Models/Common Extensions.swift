302b647365d76f0ee3aa05c2cba0cd1d990ae8d2
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-05-22 13:20:37 -0400

    add Common Extension.swift

---
+++ neuCKAN/Models/Common Extensions.swift
@@ -0,0 +1,11 @@
+//
+//  Common Extensions.swift
+//  neuCKAN
+//
+//  Created by you on 20-05-22.
+//  Copyleft © 2020 Wowbagger & His Liquid Lunch. All wrongs reserved.
+//
+
+import Foundation
+
+
