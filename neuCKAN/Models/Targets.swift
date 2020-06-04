6fa1515fe3bc7e05d64eba3474531f2a80324164
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-06-04 00:40:58 -0400

    fix documentation style before adding CollectionOfUniqueElements conformance

--- neuCKAN/Models/Targets.swift
+++ neuCKAN/Models/Targets.swift
@@ -10,24 +10,19 @@ import Foundation
 import Cocoa
 import os.log
 
-/**
-A collection of KSP installations managable by neuCKAN.
-*/
+///	A collection of KSP installations managable by neuCKAN.
 struct Targets: Hashable {
-	
-	/**
-	Initialises a collection of targets from the given sequence of target convertibles.
-	
-	Because of [a problem with generic default parameters][generic default parameters problem], use `init(groupVersion: Version, groupingLevel: GroupingLevel, conflictHandlingScheme: ConflictHandlingOption)` to initialise an empty collection of targets.
-	
-	- Parameters:
-		- targets: The sequence of target convertibles to initialise the collection of targets from.
-		- groupVersion: The shared version among the group members.
-		- groupingLevel: The level of grouping the target is initialised for. This is mostly for the convenience of the `TargetView`'s source list's data source and delegates.
-		- conflictHandlingScheme: The scheme for handeling possible targets with the same inode value in the sequence of targets.
-	
-	[generic default parameters problem]: https://stackoverflow.com/questions/38326992/default-parameter-as-generic-type
-	*/
+	///	Initialises a collection of targets from the given sequence of target convertibles.
+	///
+	///	Because of [a problem with generic default parameters][generic default parameters problem], use `init(groupVersion: Version, groupingLevel: GroupingLevel, conflictHandlingScheme: ConflictHandlingOption)` to initialise an empty collection of targets.
+	///
+	///	- Parameters:
+	///	  - targets: The sequence of target convertibles to initialise the collection of targets from.
+	///	  - groupVersion: The shared version among the group members.
+	///	  - groupingLevel: The level of grouping the target is initialised for. This is mostly for the convenience of the `TargetView`'s source list's data source and delegates.
+	///	  - conflictHandlingScheme: The scheme for handeling possible targets with the same inode value in the sequence of targets.
+	///
+	///	[generic default parameters problem]: https:stackoverflow.com/questions/38326992/default-parameter-as-generic-type
 	init<T: Sequence>(targets: T, groupVersion: Version = Version(""), groupingLevel: GroupingLevel = .none, conflictHandlingScheme: ConflictHandlingOption = .preserveFirstOccurence) where T.Element == TargetConvertible? {
 		var temporaryTargets: [Target] = []
 		targets.compactMap { $0?.asTarget() } .forEach { target in
@@ -45,18 +40,16 @@ struct Targets: Hashable {
 		self.groupingLevel = groupingLevel
 	}
 	
-	/**
-	Initialises an empty collection of targets from the given sequence of target convertibles.
-	
-	This initialiser exists because of [a problem with generic default parameters][generic default parameters problem]. Use `init<T: Sequence>(targets: T, groupVersion: Version, groupingLevel: GroupingLevel, conflictHandlingScheme: ConflictHandlingOption) where T.Element == Target` to initialise a non-empty collection of targets.
-	
-	- Parameters:
-		- groupVersion: The shared version among the group members.
-		- groupingLevel: The level of grouping the target is initialised for. This is mostly for the convenience of the `TargetView`'s source list's data source and delegates.
-		- conflictHandlingScheme: The scheme for handeling possible targets with the same inode value in the sequence of targets.
-	
-	[generic default parameters problem]: https://stackoverflow.com/questions/38326992/default-parameter-as-generic-type
-	*/
+	///	Initialises an empty collection of targets from the given sequence of target convertibles.
+	///
+	///	This initialiser exists because of [a problem with generic default parameters][generic default parameters problem]. Use `init<T: Sequence>(targets: T, groupVersion: Version, groupingLevel: GroupingLevel, conflictHandlingScheme: ConflictHandlingOption) where T.Element == Target` to initialise a non-empty collection of targets.
+	///
+	///	- Parameters:
+	///	  - groupVersion: The shared version among the group members.
+	///	  - groupingLevel: The level of grouping the target is initialised for. This is mostly for the convenience of the `TargetView`'s source list's data source and delegates.
+	///	  - conflictHandlingScheme: The scheme for handeling possible targets with the same inode value in the sequence of targets.
+	///
+	///	[generic default parameters problem]: https://stackoverflow.com/questions/38326992/default-parameter-as-generic-type
 	init(groupVersion: Version = Version(""), groupingLevel: GroupingLevel = .none, conflictHandlingScheme: ConflictHandlingOption = .preserveFirstOccurence) {
 		self.init(targets: [], groupVersion: groupVersion, groupingLevel: groupingLevel, conflictHandlingScheme: conflictHandlingScheme)
 	}
@@ -130,49 +123,31 @@ struct Targets: Hashable {
 //	MARK: - Collection Conformance
 extension Targets: Collection {
 	
-	/**
-	The position of a target in the collection of targets.
-	
-	This is the same as `Array<Target>.Index`, which is in turn a type alias for `Int`.
-	*/
+	///	The position of a target in the collection of targets.
+	///
+	///	This is the same as `Array<Target>.Index`, which is in turn a type alias for `Int`.
 	typealias Index = Array<Target>.Index
 	
-	/**
-	The position of the first target in a nonempty collection of targets.
-	
-	If the collection has no targets, `startIndex` is equal to `endIndex`.
-	
-	- See Also: `endIndex`.
-	*/
+	///	The position of the first target in a nonempty collection of targets.
+	///
+	///	If the collection has no targets, `startIndex` is equal to `endIndex`.
 	var startIndex: Index { targets.startIndex }
 	
-	/**
-	The collection of targets’ “past the end” position—i.e. the position one greater than the last valid subscript argument.
-	
-	When you need a range that includes the last target of the collection, use the half-open range operator (`..<`) with `endIndex`. The `..<` operator creates a range that doesn’t include the upper bound, so it’s always safe to use with `endIndex`.
-	
-	If the collection has no targets, `endIndex` is equal to `startIndex`.
-	
-	- See Also: `startIndex`.
-	*/
+	///	The collection of targets’ “past the end” position—i.e. the position one greater than the last valid subscript argument.
+	///
+	///	When you need a range that includes the last target of the collection, use the half-open range operator (`..<`) with `endIndex`. The `..<` operator creates a range that doesn’t include the upper bound, so it’s always safe to use with `endIndex`.
+	///
+	///	If the collection has no targets, `endIndex` is equal to `startIndex`.
 	var endIndex: Index { targets.endIndex }
 	
-	/**
-	Returns the position immediately after the given index.
-	
-	- Parameter i: A valid polition in the collection of targets. `i` must be less than `endIndex`.
-	
-	- Returns: The index value immediately after `i.`
-	
-	- See Also: `endIndex`.
-	*/
+	///	Returns the position immediately after the given index.
+	///
+	///	- Parameter i: A valid polition in the collection of targets. `i` must be less than `endIndex`.
+	///	- Returns: The index value immediately after `i.`
 	func index(after i: Index) -> Index { targets.index(after: i) }
 	
-	/**
-	Inserts a target converted from the given target convertible into the collection of targets if it is not already present.
-	
-	- Parameter newTarget: The target convertible whose converted target is to insert into the collection of targets. `nil` is ignored.
-	*/
+	///	Inserts a target converted from the given target convertible into the collection of targets if it is not already present.
+	///	- Parameter newTarget: The target convertible whose converted target is to insert into the collection of targets. `nil` is ignored.
 	mutating func insert(newElement newTarget: TargetConvertible?) {
 		guard let newTarget = newTarget?.asTarget() else { return }
 		if !contains(where: { $0.inode == newTarget.inode } ) {
@@ -180,33 +155,26 @@ extension Targets: Collection {
 		}
 	}
 	
-	/**
-	Merges targets converted from the given sequence of target convertibles into the current collection of targets, if any of them is not already present.
-	
-	When multiple new targets share the same inode, the first in order among them takes precedence.
-	
-	- Parameter newTargets: The target convertibles whose converted targets are to insert into the collection of targets. `nil`s in the sequence are ignored.
-	*/
+	///	Merges targets converted from the given sequence of target convertibles into the current collection of targets, if any of them is not already present.
+	///
+	///	When multiple new targets share the same inode, the first in order among them takes precedence.
+	///
+	///	- Parameter newTargets: The target convertibles whose converted targets are to insert into the collection of targets. `nil`s in the sequence are ignored.
 	mutating func insert<T: Sequence>(contentsOf newTargets: T) where T.Element == TargetConvertible? {
 		newTargets.forEach { insert(newElement: $0) }
 	}
 	
-	/**
-	Inserts targets at the given paths into the collection of targets if any of them is not already present.
-	
-	When multiple new targets share the same inode, the first in order among them takes precedence.
-	
-	- Parameter paths: The paths where the targets to insert into the collection of targets are. `nil`s in the sequence are ignored.
-	*/
+	///	Inserts targets at the given paths into the collection of targets if any of them is not already present.
+	///
+	///	When multiple new targets share the same inode, the first in order among them takes precedence.
+	///
+	///	- Parameter paths: The paths where the targets to insert into the collection of targets are. `nil`s in the sequence are ignored.
 	mutating func insert<T: Sequence>(newTargetsAt paths: T) where T.Element: FileURLConvertible {
 		insert(contentsOf: paths.compactMap { $0.asTarget() } )
 	}
 	
-	/**
-	Inserts a target converted from the given target convertible into the collection of targets unconditionally.
-	
-	- Parameter newTarget: The target convertible whose converted target is to insert into the collection of targets. `nil` is ignored.
-	*/
+	///	Inserts a target converted from the given target convertible into the collection of targets unconditionally.
+	///	- Parameter newTarget: The target convertible whose converted target is to insert into the collection of targets. `nil` is ignored.
 	mutating func update(newElement newTarget: TargetConvertible?) {
 		guard let newTarget = newTarget?.asTarget() else { return }
 		if let oldTargetIndex = firstIndex(where: { $0.inode == newTarget.inode } ) {
@@ -216,39 +184,30 @@ extension Targets: Collection {
 		}
 	}
 	
-	/**
-	Merges targets converted from the given sequence of target convertibles into the current collection of targets unconditionally.
-	
-	When multiple new targets share the same inode, the last in order among them takes precedence.
-	
-	- Parameter newTargets: The target convertibles whose converted targets are to insert into the collection of targets. `nil`s in the sequence are ignored.
-	*/
+	///	Merges targets converted from the given sequence of target convertibles into the current collection of targets unconditionally.
+	///
+	///	When multiple new targets share the same inode, the last in order among them takes precedence.
+	///
+	///	- Parameter newTargets: The target convertibles whose converted targets are to insert into the collection of targets. `nil`s in the sequence are ignored.
 	mutating func update<T: Sequence>(contentsOf newTargets: T) where T.Element == TargetConvertible? {
 		newTargets.forEach { update(newElement: $0) }
 	}
 	
-	/**
-	Merges targets at the given paths into the current collection of targets unconditionally.
-	
-	When multiple new targets share the same inode, the last in order among them takes precedence.
-	
-	- Parameter paths: The paths where the targets to insert into the collection of targets are. `nil`s in the sequence are ignored.
-	*/
+	///	Merges targets at the given paths into the current collection of targets unconditionally.
+	///
+	///	When multiple new targets share the same inode, the last in order among them takes precedence.
+	///
+	///	- Parameter paths: The paths where the targets to insert into the collection of targets are. `nil`s in the sequence are ignored.
 	mutating func update<T: Sequence>(newTargetsAt paths: T) where T.Element == FileURLConvertible {
 		update(contentsOf: paths.compactMap { $0.asTarget() } )
 	}
 	
-	/**
-	Accesses the mod at the specified path.
-	
-	This subscript provides read-only access.
-	
-	- Parameter path: The path of the target to access.
-	
-	- Returns: The target at the specified path.
-	
-	- Complexity: O(1).
-	*/
+	///	Accesses the mod at the specified path.
+	///
+	///	This subscript provides read-only access.
+	///
+	///	- Parameter path: The path of the target to access.
+	///	- Returns: The target at the specified path.
 	private(set) subscript(path: FileURLConvertible) -> Target? {
 		get {
 			let kspURL = path.asFileURL()
@@ -268,19 +227,12 @@ extension Targets: Collection {
 		}
 	}
 	
-	/**
-	Accesses the target at the specified position.
-	
-	This subscript provides read-only access.
-	
-	- Parameter position: The position of the target to access. `position` must be a valid index of the collection of targets, that is not equal to the `endIndex` property.
-	
-	- Returns: The target at the specified index.
-	
-	- Complexity: O(1).
-	
-	- See Also: `endIndex`.
-	*/
+	///	Accesses the target at the specified position.
+	///
+	///	This subscript provides read-only access.
+	///
+	///	- Parameter position: The position of the target to access. `position` must be a valid index of the collection of targets, that is not equal to the `endIndex` property.
+	///	- Returns: The target at the specified index.
 	private(set) subscript(position: Array<Target>.Index) -> Target {
 		get { targets[position] }
 		set(newTarget) { insert(newElement: newTarget) }
@@ -300,10 +252,7 @@ extension Targets: ExpressibleByArrayLiteral {
 
 //	MARK: - TargetsConvertible Conformance
 extension Targets: TargetsConvertible {
-	/**
-	Returns `self`.
-	
-	- Returns: `self`.
-	*/
+	///	Returns `self`.
+	///	- Returns: `self`.
 	func asTargets() -> Targets? { self }
 }
