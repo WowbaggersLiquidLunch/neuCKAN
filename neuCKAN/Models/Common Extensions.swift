772ca393754b597489b644735ef011a59f284f92
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-06-10 17:22:13 -0400

    add group<T>(by criterion: (_ transforming: Element) throws -> T)

--- neuCKAN/Models/Common Extensions.swift
+++ neuCKAN/Models/Common Extensions.swift
@@ -14,3 +14,18 @@ extension NSRegularExpression: Comparable {
 	}
 }
 
+extension Sequence {
+	//	FIXME: group<T>(by key: (Element) throws -> T) rethrows -> OrderedDictionary<T, Self> will be better.
+	//	FIXME: Add labels to closure as soon as Swift supports it.
+	/// Group the sequence by the given criterion.
+	/// - Parameter criterion: The basis by which to group the sequence.
+	/// - Throws: `Error`.
+	/// - Returns: A dictionary consisting of the sequence grouped by the given criterion.
+	func group<T>(by criterion: (_ transforming: Element) throws -> T) rethrows -> [T: [Element]] {
+		var groups: [T: [Element]] = [:]
+		for element in self {
+			try groups[criterion(_: element), default: []].append(element)
+		}
+		return groups
+	}
+}
