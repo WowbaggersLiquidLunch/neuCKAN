36b69f3ca32a03dc67dadae15f4446221a9434ec
Wowbagger's Liquid Lunch <55120045+WowbaggersLiquidLunch@users.noreply.github.com>
2020-06-10 19:37:46 -0400

    eliminate cache problem by replacing URLImage with KFImage

--- neuCKAN/Views/ModPreviewSlide.swift
+++ neuCKAN/Views/ModPreviewSlide.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 import URLImage
+import KingfisherSwiftUI
 
 ///	A scrollable preview of the mod release.
 struct ModPreviewSlide: View {
@@ -25,27 +26,37 @@ struct ModPreviewSlide: View {
 			
 			ScrollView(.horizontal, showsIndicators: true) {
 				if release.resources?.screenshot != nil {
-					URLImage(release.resources!.screenshot!, incremental: true, placeholder: {
-						ProgressView($0) { progress in
-							ZStack {
-								if progress > 0.0 {
-									CircleProgressView(progress).stroke(lineWidth: 2.0)
-								}
-								else {
-									CircleActivityView().stroke(lineWidth: 2.0)
-								}
-							}
-						}
-						.frame(width: 25.0, height: 25.0)
-					}, content: { proxy in
-						proxy.image
-							.antialiased(true)
-							.resizable()
-							.scaledToFill()
-					})
+//					URLImage(release.resources!.screenshot!, incremental: true, placeholder: {
+//						ProgressView($0) { progress in
+//							ZStack {
+//								if progress > 0.0 {
+//									CircleProgressView(progress).stroke(lineWidth: 2.0)
+//								}
+//								else {
+//									CircleActivityView().stroke(lineWidth: 2.0)
+//								}
+//							}
+//						}
+//						.frame(width: 25.0, height: 25.0)
+//					}, content: { proxy in
+//						proxy.image
+//							.antialiased(true)
+//							.resizable()
+//							.scaledToFill()
+//					})
+//						.frame(width: 430, height: 270, alignment: .center)
+//						.cornerRadius(5)
+					
+					//	TODO: Add placeholders/indicators or show/hide the entire preview based on the network request result.
+					KFImage(
+						release.resources!.screenshot!,
+						options: [.backgroundDecode, .progressiveJPEG(.default)]
+					)
+						.antialiased(true)
+						.resizable()
+						.scaledToFill()
 						.frame(width: 430, height: 270, alignment: .center)
 						.cornerRadius(5)
-
 				}
 			}
 		}
